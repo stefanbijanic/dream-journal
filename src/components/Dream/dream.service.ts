73fa65e275bb3510b964a613173031c5a4c3aafe
stefanbijanic <bijanic32@gmail.com>
2022-08-15 16:27:48 +0200

    Added error handling responses

--- src/components/Dream/dream.service.ts
+++ src/components/Dream/dream.service.ts
@@ -1,18 +1,27 @@
+import {DeleteResult} from "typeorm";
+import ErrorResponse from "../../common/error-response.interface";
 import {Dream, DreamTypes} from "./dream.entity";
 import {CreateDreamDto} from "./dto/create-dream.dto";
 import {EditDreamDto} from "./dto/edit-dream.dto";
 
 export default class DreamService {
-    public async getAllDreams() {
-        const dreams = await Dream.find()
+    public async getAllDreams(): Promise<Dream[] | ErrorResponse> {
+        try {
+            const dreams = await Dream.find()
         return dreams;
+        } catch (error) {
+            return({
+                errorCode: error?.errno,
+                errorMessage: error?.sqlMessage,
+            })
+        }
     }
 
     public async getAllDreamTypes() {
         return Object.values(DreamTypes);
     }
 
-    public async createDream(data: CreateDreamDto) {
+    public async createDream(data: CreateDreamDto): Promise<Dream | ErrorResponse> {
         try {
             const dream = Dream.create({
                 dream_name: data.dreamName,
@@ -25,27 +34,47 @@ export default class DreamService {
 
             return dream;
         } catch (error) {
-
+            return({
+                errorCode: error?.errno,
+                errorMessage: error?.sqlMessage,
+            })
         }
     }
 
-    public async getDreamById(dreamId: number) {
+    public async getDreamById(dreamId: number): Promise<Dream | ErrorResponse> {
         try {
             const dream = await Dream.findOneBy({
                 dream_id: dreamId,
             })
-
+            
+            if(!dream) {
+                return({
+                    errorCode: 1001,
+                    errorMessage: "Invalid Dream ID",
+                })
+            }
+            
             return dream;
         } catch (error) {
-            
+            return({
+                errorCode: error?.errno,
+                errorMessage: error?.sqlMessage,
+            })
         }
     }
 
-    public async editDreamById(dreamId: number, data: EditDreamDto) {
+    public async editDreamById(dreamId: number, data: EditDreamDto): Promise<Dream | ErrorResponse> {
         try {
             const dream = await Dream.findOneBy({
                 dream_id: dreamId,
             })
+
+            if(!dream) {
+                return({
+                    errorCode: 1001,
+                    errorMessage: "Invalid Dream ID",
+                })
+            }
 
             dream.dream_name = data.dreamName;
             dream.dream_type = data.dreamType;
@@ -56,11 +85,14 @@ export default class DreamService {
 
             return dream;
         } catch (error) {
-            
+            return({
+                errorCode: error?.errno,
+                errorMessage: error?.sqlMessage,
+            })
         }
     }
 
-    public async deleteDreamById(dreamId: number) {
+    public async deleteDreamById(dreamId: number): Promise<DeleteResult | ErrorResponse> {
         try {
             const result = await Dream.delete({
                 dream_id: dreamId,
@@ -68,7 +100,10 @@ export default class DreamService {
 
             return result;
         } catch (error) {
-            
+            return({
+                errorCode: error?.errno,
+                errorMessage: error?.sqlMessage,
+            })
         }
     }
 }
